4a7e9121a676569d9c662112388afb805f1da354
bkurniawan <bob.kurniawan@gmail.com>
2017-05-15 15:07:17 +0700

    test Refactor
    test Refactor

--- src/java/eu/sig/training/ch04/CheckingAccount.java
+++ src/java/eu/sig/training/ch04/CheckingAccount.java
@@ -8,26 +8,43 @@ public class CheckingAccount {
 
     public Transfer makeTransfer(String counterAccount, Money amount)
         throws BusinessException {
-        // 1. Check withdrawal limit:
-        if (amount.greaterThan(this.transferLimit)) {
-            throw new BusinessException("Limit exceeded!");
-        }
+        checkWithdrawalLimit(amount);
         // 2. Assuming result is 9-digit bank account number, validate 11-test:
-        int sum = 0;
+        return validate11(counterAccount, amount);
+    }
+
+
+	private Transfer validate11(String counterAccount, Money amount) throws BusinessException {
+		int sum = 0;
         for (int i = 0; i < counterAccount.length(); i++) {
             char character = counterAccount.charAt(i);
             int characterValue = Character.getNumericValue(character);
             sum = sum + (9 - i) * characterValue;
         }
         if (sum % 11 == 0) {
-            // 3. Look up counter account and make transfer object:
-            CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
-            Transfer result = new Transfer(this, acct, amount);
-            return result;
+            return lookUpCounterAccAndTransferObject(counterAccount, amount);
         } else {
             throw new BusinessException("Invalid account number!");
         }
-    }
+	}
+
+	private Transfer lookUpCounterAccAndTransferObject(String counterAccount, Money amount) {
+		CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
+		Transfer result = new Transfer(this, acct, amount);
+		return result;
+	}
+
+
+
+
+	private void checkWithdrawalLimit(Money amount) throws BusinessException {
+		if (amount.greaterThan(this.transferLimit)) {
+            throw new BusinessException("Limit exceeded!");
+        }
+	}
+    
+    
+   
 
     public void addInterest() {
         Money interest = balance.multiply(INTEREST_PERCENTAGE);
